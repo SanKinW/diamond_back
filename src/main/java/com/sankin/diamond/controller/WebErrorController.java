f6a4079f44994410a672de871495322cb6baf86f
SanKinW <18373275@buaa.edu.cn>
2020-08-18 19:39:51 +0800

    '自定义错误页面'

--- src/main/java/com/sankin/diamond/controller/WebErrorController.java
+++ src/main/java/com/sankin/diamond/controller/WebErrorController.java
@@ -6,11 +6,12 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@RestController
 @RequestMapping("${server.error.path:${error.path:/error}}")
 public class WebErrorController implements ErrorController {
     @Override
@@ -18,18 +19,16 @@ public class WebErrorController implements ErrorController {
         return null;
     }
 
-    /*
     @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView errorHtml(HttpServletRequest request, Model model) {
-        HttpStatus status = getStatus(request);
-
+    public String errorHtml(HttpServletRequest request, Model model) {
+        /*HttpStatus status = getStatus(request);
         if (status.is4xxClientError()) {
             model.addAttribute("message","您的请求出错了，要不换个姿势吧");
         }
         if (status.is5xxServerError()) {
             model.addAttribute("message","服务器冒烟了，请稍后再试！");
-        }
-        return new ModelAndView("error");
+        }*/
+        return "404 Not Found";
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
@@ -43,5 +42,4 @@ public class WebErrorController implements ErrorController {
             return HttpStatus.INTERNAL_SERVER_ERROR;
         }
     }
-    */
 }
